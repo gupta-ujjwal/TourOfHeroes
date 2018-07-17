bf1a1a7d0e1d0e75fc5468c626f14bc703cdc082
Ujjwal Gupta <gupta.ujjwal14@gmail.com>
2018-07-18 00:17:39 +0530

    Seperated the hero-detail page and linked it with dashboard and hero-component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,11 +7,10 @@ import { HeroService } from '../hero.service'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  constructor(private heroService : HeroService) { 
-  }
-  heroes : Hero[];
-  selectedHero : Hero;
-  
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
   ngOnInit() {
     this.getHeroes();
   }
@@ -19,10 +18,6 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
 }
